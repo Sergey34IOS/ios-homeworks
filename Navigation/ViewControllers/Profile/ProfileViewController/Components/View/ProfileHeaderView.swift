754cfc62d247a0ef7ef78bc627f256f073068fa9
Sergey <tomyettimcneal@gmail.com>
2023-04-11 22:05:39 +0300

    Animation added

--- Navigation/ViewControllers/Profile/ProfileViewController/Components/View/ProfileHeaderView.swift
+++ Navigation/ViewControllers/Profile/ProfileViewController/Components/View/ProfileHeaderView.swift
@@ -8,8 +8,10 @@
 import UIKit
 
 final class ProfileHeaderView: UIView {
-
-    private var catImage: UIImageView = {
+    
+    //    MARK: - Properties
+    
+    private lazy var catImage: UIImageView = {
         var catImage = UIImageView()
         catImage.image = UIImage(named: "felix")
         catImage.clipsToBounds = true
@@ -17,7 +19,26 @@ final class ProfileHeaderView: UIView {
         catImage.layer.cornerRadius = 65
         catImage.layer.borderWidth = 3
         catImage.layer.borderColor = UIColor.white.cgColor
+        catImage.isUserInteractionEnabled = true
+        catImage.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(tapImageAction)))
+        catImage.translatesAutoresizingMaskIntoConstraints = false
         return catImage
+    }()
+    
+    private var transparentView: UIView = {
+        let view = UIView(frame: CGRect(x: 0, y: -100, width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height))
+        view.backgroundColor = .black
+        view.alpha = 0
+        return view
+    }()
+    
+    private var xButton: UIButton = {
+        let button = UIButton()
+        button.setImage(UIImage(systemName: "xmark"), for: .normal)
+        button.alpha = 0.5
+        button.tintColor = .white
+        button.addTarget(self, action: #selector(closeAvatar), for: .touchUpInside)
+        return button
     }()
     
     private var nameLabel: UILabel = {
@@ -36,6 +57,8 @@ final class ProfileHeaderView: UIView {
     
     private var statusText: String = ""
     
+    private lazy var tabBar = ((superview as? UITableView)?.dataSource as? UIViewController)?.tabBarController?.tabBar
+    
     private var statusTextField: UITextField = {
         var statusTextField = UITextField()
         statusTextField.backgroundColor = .white
@@ -49,7 +72,7 @@ final class ProfileHeaderView: UIView {
         statusTextField.leftViewMode = .always
         return statusTextField
     }()
-
+    
     private var statusButton: UIButton = {
         var statusButton = UIButton()
         statusButton.layer.cornerRadius = 12
@@ -64,22 +87,25 @@ final class ProfileHeaderView: UIView {
         return statusButton
     }()
     
+    private lazy var avatarCenter = catImage.center
+    private lazy var avatarBounds = catImage.layer.bounds
+    
     // MARK: - Initializers
     
-   override init(frame: CGRect) {
+    override init(frame: CGRect) {
         super.init(frame: .zero)
         backgroundColor = .systemGray6
         addSubviews()
         setTargets()
         addConstraints()
         addRecognizer()
-       
-       let currentStatus = UserDefaults.standard.string(forKey: "someValue")
-       if currentStatus == "" {
-           self.statusLabel.text = "Print something..."
-       } else {
-           self.statusLabel.text = currentStatus
-       }
+        
+        let currentStatus = UserDefaults.standard.string(forKey: "someValue")
+        if currentStatus == "" {
+            self.statusLabel.text = "Print something..."
+        } else {
+            self.statusLabel.text = currentStatus
+        }
     }
     
     @available(*, unavailable)
@@ -88,6 +114,52 @@ final class ProfileHeaderView: UIView {
     }
     
     // MARK: - Action
+    
+    
+    @objc
+    private func tapImageAction() {
+        UIImageView.animate(withDuration: 0.5,
+                            delay: 0.1
+        ) { [self] in
+            transparentView.alpha = 0.5
+            catImage.layer.borderWidth = 0
+            catImage.layer.cornerRadius = 0
+            catImage.center = transparentView.center
+            catImage.layer.bounds = CGRect(x: 0, y: 0, width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.width)
+            tabBar?.frame.origin.y = UIScreen.main.bounds.height
+        }
+    completion: { _ in
+        UIView.animate(withDuration: 0.5) { [weak self] in
+            self?.xButton.alpha = 1
+        }
+    }
+    }
+    @objc
+    private func closeAvatar() {
+        UIView.animate(withDuration: 0.3) { [self] in
+            xButton.alpha = 0
+        } completion: { _ in
+            UIView.animate(withDuration: 0.3
+                           //                           usingSpringWithDamping: 0.5,
+                           //                           initialSpringVelocity: 1,
+                           //                           options: .curveEaseInOut
+                           
+            ) { [self] in
+                transparentView.alpha = 0
+                catImage.layer.borderWidth = 3
+                catImage.layer.cornerRadius = 65
+                catImage.clipsToBounds = true
+                catImage.center = avatarCenter
+                catImage.bounds = avatarBounds
+                self.setNeedsLayout()
+                //                self.layoutIfNeeded()
+                self.updateConstraints()
+                if let bar = tabBar {
+                    bar.frame.origin.y = UIScreen.main.bounds.height - bar.frame.height
+                }
+            }
+        }
+    }
     
     @objc
     func statusTextChanged(_ textField: UITextField) {
@@ -127,11 +199,15 @@ final class ProfileHeaderView: UIView {
 extension ProfileHeaderView {
     
     private func addSubviews() {
-        addSubview(catImage)
+        
         addSubview(nameLabel)
         addSubview(statusLabel)
         addSubview(statusTextField)
         addSubview(statusButton)
+        addSubview(transparentView)
+        addSubview(xButton)
+        addSubview(catImage)
+        
     }
     
     private func setTargets() {
@@ -141,7 +217,7 @@ extension ProfileHeaderView {
     
     private func addConstraints() {
         
-        [catImage, nameLabel, statusButton, statusLabel, statusTextField].forEach {
+        [xButton, catImage, nameLabel, statusButton, statusLabel, statusTextField].forEach {
             $0.translatesAutoresizingMaskIntoConstraints = false
         }
         
@@ -166,7 +242,11 @@ extension ProfileHeaderView {
             statusButton.leftAnchor.constraint(equalTo: leftAnchor, constant: 16),
             statusButton.rightAnchor.constraint(equalTo: self.rightAnchor, constant: -16),
             statusButton.heightAnchor.constraint(equalToConstant: 50),
-            statusButton.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: -5)
+            statusButton.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: -5),
+            
+            xButton.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor, constant: 16),
+            xButton.rightAnchor.constraint(equalTo: statusButton.rightAnchor, constant: -16),
+            xButton.heightAnchor.constraint(equalToConstant: 50)
         ])
     }
 }
